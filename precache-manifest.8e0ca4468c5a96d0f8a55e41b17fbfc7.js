2c6ca4234eee8a21e7e04a2a904e333ac53db0a6
Hee Su <kessris@hotmail.com>
2019-11-23 15:48:32 +0900

    Updates

---
+++ precache-manifest.8e0ca4468c5a96d0f8a55e41b17fbfc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22cac067a96d25469cae0a2ec368059",
+    "url": "/react-practice-monster/index.html"
+  },
+  {
+    "revision": "63aa9e2ad2f858ed00cc",
+    "url": "/react-practice-monster/static/css/main.e0e5d313.chunk.css"
+  },
+  {
+    "revision": "ebc70e04cd919c0bd6e0",
+    "url": "/react-practice-monster/static/js/2.35df3c83.chunk.js"
+  },
+  {
+    "revision": "63aa9e2ad2f858ed00cc",
+    "url": "/react-practice-monster/static/js/main.c3d98b23.chunk.js"
+  },
+  {
+    "revision": "32471b901e0c1abd68de",
+    "url": "/react-practice-monster/static/js/runtime-main.4407683b.js"
+  }
+]);
